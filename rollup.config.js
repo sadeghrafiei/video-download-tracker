9e5bc6265c32a2a7083e0752dfe31fe7a895d2f9
sadegh.rafiei <sadeghrafiei80@gmail.com>
2025-08-14 17:41:28 +0330

    chore: Optimize package size

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,49 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: 'dist/index.js',
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: 'dist/index.esm.js',
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({
-                tsconfig: './tsconfig.json',
-                declaration: true,
-                declarationDir: './dist',
-            }),
-        ],
-        external: [],
-    },
-    {
-        input: 'dist/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-    },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: true,
+        declarationDir: "./dist",
+        outDir: "./dist",
+      }),
+      terser({
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          pure_funcs: ["console.log", "console.info", "console.debug"],
+        },
+        mangle: {
+          toplevel: true,
+        },
+      }),
+    ],
+    external: ["tslib"],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
